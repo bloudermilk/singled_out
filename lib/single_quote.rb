b167ebf3880fdb3ec6ab48cd3370d8c19d97a63b
Brendan Loudermilk <brendan@gophilosophie.com>
2012-12-04 15:15:41 -0800

    Glob and fix

--- lib/single_quote.rb
+++ lib/single_quote.rb
@@ -2,10 +2,6 @@ require "single_quote/source_file"
 require "single_quote/version"
 
 module SingleQuote
-  def self.fix_glob(glob)
-    Dir[glob].each { |f| fix_file(f) }
-  end
-
   def self.fix_file(file)
     contents = File.read(file)
     source_file = SourceFile.new(contents)
